238e6c2fc983cde58529646913c01a96cd85bbf4
the-metapod <novitzkimain@gmail.com>
2023-01-30 22:07:12 +0100

    moved main classes

---
+++ src/main/java/TowerDefence.java
@@ -0,0 +1,33 @@
+import game.engine.ConfigurableGameEngine;
+import game.engine.EngineConfig;
+import game.engine.GameEngine;
+import presentation.components.GUI;
+import presentation.loaders.map.TextFileCastleLoader;
+import presentation.loaders.map.TextFileMapLoader;
+import presentation.loaders.sprites.CreatureResourceAnimationFactory;
+import presentation.loaders.sprites.TowerResourceSpriteFactory;
+
+public class TowerDefence {
+
+    public static void main(String[] args) {
+        final EngineConfig engineConfig = getEngineConfiguration();
+        final GameEngine gameEngine = ConfigurableGameEngine.configure(engineConfig);
+        final GUI gui = new GUI(gameEngine);
+        gameEngine.start();
+    }
+
+    private static EngineConfig getEngineConfiguration() {
+        final TextFileCastleLoader castleLoader = new TextFileCastleLoader();
+        final TextFileMapLoader mapLoader = new TextFileMapLoader();
+        final CreatureResourceAnimationFactory creatureAnimationFactory = new CreatureResourceAnimationFactory();
+        final TowerResourceSpriteFactory towerSpriteFactory = new TowerResourceSpriteFactory();
+
+        return EngineConfig.builder()
+                .castleLoader(castleLoader)
+                .mapLoader(mapLoader)
+                .monsterAnimationFactory(creatureAnimationFactory)
+                .soldierAnimationFactory(creatureAnimationFactory)
+                .towerSpriteFactory(towerSpriteFactory)
+                .build();
+    }
+}
--- src/main/java/presentation/TowerDefence.java
+++
@@ -1,8 +0,0 @@
-package presentation;
-
-public class TowerDefence {
-
-    public static void main(String[] args) {
-
-    }
-}
